62d682e28fdae9d5a4eaaa09c09c98696be0298b
Midnightess <asm1@bk.ru>
2023-11-08 01:29:15 +0300

    Функция генерации временных координат

---
+++ js/functions.js
@@ -0,0 +1,36 @@
+const isPalindrom = (str) => {
+  const normalizedStr = str.toLowerCase().replaceAll(' ', '');
+  return normalizedStr === normalizedStr.split('').reverse().join('');
+};
+
+isPalindrom('Лёша на полке клопа нашёл');
+
+
+const extractNumber = (str) => {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (!Number.isNaN(parseInt(str.at(i), 10))) {
+      result += str.at(i);
+    }
+  }
+  return parseInt(result, 10);
+};
+
+extractNumber('ECMAScript 2023');
+
+
+const myPadStart = (str, minLength, pad) => {
+  const actualPad = minLength - str.length;
+  return (actualPad <= 0) ? str : pad.slice (0, actualPad % pad.length)
++ pad.repeat (actualPad / pad.length) + str;
+};
+
+myPadStart('q', 4, 'we');
+
+
+const getRandomCoordinates = (min, max, decimal) => {
+  const random = Math.random() * (max - min + 1) + min;
+  return random.toFixed(decimal);
+};
+
+console.log(getRandomCoordinates(6, -10, 66,6));
